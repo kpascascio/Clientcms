2e80682482f28d7412a83cc7c341bd7075c5ac3d
Kenn Pascascio <kenn370@gmail.com>
2017-08-03 16:24:35 -0400

    connected firebase to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// Angular fire imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+// Component imports
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+// services
+import { ClientService } from './services/client.service';
+
+const appRoutes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCa7JKr79OH43PMHCiHZkeu4z6jSt9nuFo',
+    authDomain: 'clientpanel-f8c20.firebaseapp.com',
+    databaseURL: 'https://clientpanel-f8c20.firebaseio.com',
+    storageBucket: 'clientpanel-f8c20.appspot.com',
+    messagingSenderId: '23939859633'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    ClientService,
+    AngularFireDatabase,
+    AngularFireAuth
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/clients/clients.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/Client';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+  clients: Client[];
+  constructor(public clientService: ClientService) { }
+
+  ngOnInit() {
+    this.clientService.getClients().subscribe(clients => {
+      console.log(clients);
+      this.clients = clients;
+      console.log(this.clients);
+    });
+    console.log(this.clients);
+  }
+}
